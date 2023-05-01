2d4e970a33aff887a0238419f26d7eda0a0da9b5
Josh Trebilco <bitbucket@trebilco.net>
2023-05-01 16:47:55 +1200

    website content

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
-<template>
+<script setup>
+import background from './assets/grass.jpg'
+import logo from './assets/logo.png'
 
+</script>
+
+<template>
+  <div class="h-screen bg-cover bg-no-repeat bg-center" :style="{ backgroundImage: `url(${background})` }">
+    <div class="flex flex-col items-center justify-center h-full">
+      <img :src="logo" alt="Minecraft NZ Logo" />
+    </div>
+  </div>
 </template>
 
